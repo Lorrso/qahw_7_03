33b89bea1974861067719b22f4d212a17f0159e1
lorrso <anreich0@gmail.com>
2024-05-15 19:28:10 +0300

    Первое задание было доработано

--- jest/test/unit/sortByName.test.js
+++ jest/test/unit/sortByName.test.js
@@ -14,25 +14,14 @@ describe("Books names test suit", () => {
     expect(output).toEqual(expected);
   });
 
-  it("Books names should be sorted in ascending order", () => {
+  it("Books names should be sorted in descending order", () => {
     const input = [
-      "Властелин Колец",
       "Алеша Попович и Тугарин Змей",
-    ];
-    const expected = [
-      "Алеша Попович и Тугарин Змей",
-      "Властелин Колец",
-    ];
-    const output = sorting.sortByName(input);
-    expect(output).toEqual(expected);
-  });
-
-  it("Book names should be sorted in descending order", () => {
-    const input = [
       "Гарри Поттер",
       "Властелин Колец",
     ];
     const expected = [
+      "Алеша Попович и Тугарин Змей",
       "Властелин Колец",
       "Гарри Поттер",
     ];
